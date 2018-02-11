7ef0a6cdd6c778e0172bdb2205f0c4959f852d3d
Dan DeRemer <dan.deremer@gmail.com>
2018-02-11 17:49:42 -0500

    Added static analysis reporting for Checkstyle.

--- jobs/build-glowstone.groovy
+++ jobs/build-glowstone.groovy
@@ -1,24 +1,23 @@
 node {
    def mvnHome
-   stage('Preparation') { // for display purposes
+   stage('Preparation') {
       // Get some code from a GitHub repository
       git branch: 'dev', 
           url: 'https://github.com/GlowstoneMC/Glowstone.git'
       // Get the Maven tool.
-      // ** NOTE: This 'M3' Maven tool must be configured
-      // **       in the global configuration.           
-      mvnHome = tool 'M3'
+      // ** NOTE: Configured in Global Tool Configuration   
+      mvnHome = tool 'Maven-3.5.2'
    }
    stage('Build') {
       // Run the maven build
-
-          if (isUnix()) {
             sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-
             archiveArtifacts artifacts: 'target/glowstone.jar',
                                         fingerprint: true
             junit 'target/surefire-reports/**/*.xml'
           }
+    stage('Analysis') {
+        def checkstyle = scanForIssues tool: [$class: 'CheckStyle'], pattern: 'target/checkstyle-result.xml'
+        publishIssues issues:[checkstyle], useStableBuildAsReference: true
+    }
 
-   }
 }
